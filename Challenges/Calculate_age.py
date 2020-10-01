c25b30c1c986239611c9d70acbccd8a33877ec46
GeorgeBrooder <Georgebrooder@gmail.com>
2020-09-30 18:31:45 -0700

    finished the challenge

---
+++ Challenges/Calculate_age.py
@@ -0,0 +1,12 @@
+# def calculate_age(current_year, birth_year):
+#   age = current_year - birth_year
+#   return age
+
+# current_year = 2049
+# birth_year = 1953
+# print(calculate_age(2049, 1953))
+
+x = 46
+y = 96
+
+print("I am " + (str(x)) + " years old and my dad is " + (str(y)) + " years old") 
